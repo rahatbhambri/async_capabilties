64a4ac63622e48d8c762e6edb2ca1e397adfd5e4
TwistedPair <35606899+rahatbhambri@users.noreply.github.com>
2024-01-06 21:59:08 +0530

    Create get_url_data.py

---
+++ get_url_data.py
@@ -0,0 +1,24 @@
+import time, asyncio as asyc
+import requests as rq
+import aiohttp as asycHttp
+
+
+async def waitedCompute(s):
+  for i in range(5):
+    time.sleep(2)
+    yield i * (s)
+
+
+async def summation():
+  session = asycHttp.ClientSession()
+  async with session.get("https://www.google.com") as resp:
+    it = waitedCompute(str(resp)[:50]).__aiter__()
+    try:
+      while True:
+        s = await it.__anext__()
+        print(s)
+    except Exception as e:
+      print(e, "ascs")
+
+
+asyc.run(summation())
